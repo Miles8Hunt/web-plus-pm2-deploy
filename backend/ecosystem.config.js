1a9b348fce79236d8b9b21405e9045f6f0d99303
Ivan Sukhovarov <isukhovarov8@gmail.com>
2024-07-03 09:15:29 +0300

    fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,8 +1,6 @@
 require('dotenv').config({ path: './.env.deploy' });
 
-const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master', DEPLOY_REPO,
-} = process.env;
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF } = process.env;
 
 module.exports = {
   apps: [{
@@ -18,7 +16,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build`,
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
